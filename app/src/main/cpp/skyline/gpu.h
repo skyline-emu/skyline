f11caa3c9e70b721f64fb779ba56a2b4a063e7c3
sanjay9867 <45529353+sanjay9867@users.noreply.github.com>
2021-05-10 17:02:44 +0530

    Update gpu.h

--- app/src/main/cpp/skyline/gpu.h
+++ app/src/main/cpp/skyline/gpu.h
@@ -5,6 +5,89 @@
 
 #include "gpu/presentation_engine.h"
 
+using CacheAddr = std::uintptr_t;
+[[nodiscard]] inline CacheAddr ToCacheAddr(const void* host_ptr) {
+    return reinterpret_cast<CacheAddr>(host_ptr);
+}
+
+[[nodiscard]] inline u8* FromCacheAddr(CacheAddr cache_addr) {
+    return reinterpret_cast<u8*>(cache_addr);
+}
+
+namespace Core {
+namespace Frontend {
+class EmuWindow;
+}
+class System;
+} // namespace Core
+
+namespace VideoCore {
+class RendererBase;
+class ShaderNotify;
+} // namespace VideoCore
+
+namespace Tegra {
+
+enum class RenderTargetFormat : u32 {
+    NONE = 0x0,
+    R32B32G32A32_FLOAT = 0xC0,
+    R32G32B32A32_SINT = 0xC1,
+    R32G32B32A32_UINT = 0xC2,
+    R16G16B16A16_UNORM = 0xC6,
+    R16G16B16A16_SNORM = 0xC7,
+    R16G16B16A16_SINT = 0xC8,
+    R16G16B16A16_UINT = 0xC9,
+    R16G16B16A16_FLOAT = 0xCA,
+    R32G32_FLOAT = 0xCB,
+    R32G32_SINT = 0xCC,
+    R32G32_UINT = 0xCD,
+    R16G16B16X16_FLOAT = 0xCE,
+    B8G8R8A8_UNORM = 0xCF,
+    B8G8R8A8_SRGB = 0xD0,
+    A2B10G10R10_UNORM = 0xD1,
+    A2B10G10R10_UINT = 0xD2,
+    A8B8G8R8_UNORM = 0xD5,
+    A8B8G8R8_SRGB = 0xD6,
+    A8B8G8R8_SNORM = 0xD7,
+    A8B8G8R8_SINT = 0xD8,
+    A8B8G8R8_UINT = 0xD9,
+    R16G16_UNORM = 0xDA,
+    R16G16_SNORM = 0xDB,
+    R16G16_SINT = 0xDC,
+    R16G16_UINT = 0xDD,
+    R16G16_FLOAT = 0xDE,
+    B10G11R11_FLOAT = 0xE0,
+    R32_SINT = 0xE3,
+    R32_UINT = 0xE4,
+    R32_FLOAT = 0xE5,
+    R5G6B5_UNORM = 0xE8,
+    A1R5G5B5_UNORM = 0xE9,
+    R8G8_UNORM = 0xEA,
+    R8G8_SNORM = 0xEB,
+    R8G8_SINT = 0xEC,
+    R8G8_UINT = 0xED,
+    R16_UNORM = 0xEE,
+    R16_SNORM = 0xEF,
+    R16_SINT = 0xF0,
+    R16_UINT = 0xF1,
+    R16_FLOAT = 0xF2,
+    R8_UNORM = 0xF3,
+    R8_SNORM = 0xF4,
+    R8_SINT = 0xF5,
+    R8_UINT = 0xF6,
+};
+
+enum class DepthFormat : u32 {
+    D32_FLOAT = 0xA,
+    D16_UNORM = 0x13,
+    S8_UINT_Z24_UNORM = 0x14,
+    D24X8_UNORM = 0x15,
+    D24S8_UNORM = 0x16,
+    D24C8_UNORM = 0x18,
+    D32_FLOAT_S8X24_UINT = 0x19,
+};
+
+
 namespace skyline::gpu {
     /**
      * @brief An interface to host GPU structures, anything concerning host GPU/Presentation APIs is encapsulated by this
